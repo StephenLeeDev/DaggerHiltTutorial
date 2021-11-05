e4df710392b9bc80ccb0437c8fadd4665d6360f2
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-11-05 22:57:57 +0900

    Add | Generate Retrofit Object

---
+++ app/src/main/java/com/example/daggerhilttutorial/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.daggerhilttutorial
+
+object Constant {
+    const val BASE_URL = "https://howtodoandroid.com/"
+}
---
+++ app/src/main/java/com/example/daggerhilttutorial/NetworkModule.kt
@@ -0,0 +1,23 @@
+package com.example.daggerhilttutorial
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://howtodoandroid.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+}
